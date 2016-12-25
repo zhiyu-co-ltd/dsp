a363f83266515f1e19384536d0ad47ba888c0a9a
iceriver2000@aliyun.com <hongxiny@oupeng.com>
2016-12-25 23:29:03 +0800

    mybatis config and spring boot update

--- src/main/java/com/zhiyu/Application.java
+++ src/main/java/com/zhiyu/Application.java
@@ -2,16 +2,18 @@ package com.zhiyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by bruce on 16/12/19.
  */
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-public class Application {
+@EnableWebMvc
+@SpringBootApplication
+public class Application extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/zhiyu/controller/testSpring.java
+++
@@ -1,7 +0,0 @@
-package com.zhiyu.controller;
-
-/**
- * Created by zhaojianfan on 16/12/21.
- */
-public class testSpring {
-}
